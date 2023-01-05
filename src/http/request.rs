a51e8e3490fc7c9c300690788bc49c94b9140bd8
Luis Osorio <yovendielmundo@gmail.com>
2023-01-05 13:14:44 +0100

    add response functionality

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,5 @@
-use super::method::Method;
-use super::method::MethodError;
+use super::method::{Method, MethodError};
+use super::QueryString;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::Debug;
@@ -9,20 +9,22 @@ use std::fmt::Result as FmtResult;
 use std::str;
 use std::str::Utf8Error;
 
+#[derive(Debug)]
 pub struct Request<'buf> {
     path: &'buf str,
-    query_string: Option<&'buf str>,
+    query_string: Option<QueryString<'buf>>,
     method: Method,
 }
 
 impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
-    fn try_from<'a>(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
+    // GET /search?name=abc&sort=1 HTTP/1.1\r\n
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
         let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         println!("{}, {}, {}", method, path, protocol);
@@ -31,24 +33,25 @@ impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
             return Err(ParseError::InvalidProtocol);
         }
         let method: Method = method.parse()?;
+
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(QueryString::from(&path[i + 1..]));
+            path = &path[..i]
+        }
+
         Ok(Self {
-            path: path,
-            query_string: Some(path),
-            method: method,
+            path,
+            query_string,
+            method,
         })
     }
 }
 
 fn get_next_word(request: &str) -> Option<(&str, &str)> {
-    // let index = request.find(|c: char| c == ' ' || c == '\r')?;
-    // Some((&request[..index], &request[index + 1..]));
-
-    for (i, c) in request.chars().enumerate() {
-        if c == ' ' || c == '\r' {
-            return Some((&request[..i], &request[i + 1..]));
-        }
-    }
-    None
+    request
+        .find(|c: char| c == ' ' || c == '\r')
+        .map(|i: usize| (&request[..i], &request[i + 1..]))
 }
 
 pub enum ParseError {
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
-use crate::http::Request;
-use std::{io::Read, net::TcpListener};
+use std::{io::Read, io::Write, net::TcpListener};
+
+use crate::http::{Request, Response, StatusCode};
 
 pub struct Server {
     addr: String,
@@ -12,7 +13,7 @@ impl Server {
 
     pub fn run(self) {
         // fn run takes ownership of the struct because
-        // of the absence of & before `self` The object will be deallocated when the funtions ends
+        // of the absence of & before `self` The object will be deallocated when the functions ends
         println!("Listening on {}", self.addr);
         let listener = TcpListener::bind(&self.addr).unwrap();
 
@@ -28,17 +29,29 @@ impl Server {
                                 nbytes
                             );
                             println!("{}", String::from_utf8_lossy(&buf));
-                            match Request::try_from(&buf[..]) {
-                                Ok(_) => {}
-                                Err(e) => {
-                                    println!("Failed to parse the request: {}", e)
+
+                            let response = match Request::try_from(&buf[..]) {
+                                Ok(request) => {
+                                    dbg!(request);
+                                    Response::new(
+                                        StatusCode::Ok,
+                                        Some("<h1>It Works!!</h1>".to_string()),
+                                    )
                                 }
+                                Err(e) => {
+                                    println!("Failed to parse the request: {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e)
                             }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
                 }
-                Err(e) => println!("Failed to stablish a connection: {}", e),
+                Err(e) => println!("Failed to establish a connection: {}", e),
             }
         }
     }
